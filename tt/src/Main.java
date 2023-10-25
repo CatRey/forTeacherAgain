674597fca2c93c8f270cbed1a7888069e0d9836e
CatRey <interes_tv@inbox.ru>
2023-10-25 21:15:07 +0300

    Task 2

--- tt/src/Main.java
+++ tt/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        Person person = new Person(23);
+       Student student = new Student();
        person.name="SHAA";
        person.age=18;
         person.printInfo();
